14c0ae7bd5c36ecec5e5219398fe73bd5316eb8d
Mohammd Shihab Hossain <68942280+Zul-Qarnain@users.noreply.github.com>
2024-05-16 16:49:28 +0600

    Allhamduliha

---
+++ Codechef/Array/RUNCOMPARE.cpp
@@ -0,0 +1,24 @@
+//https://www.codechef.com/practice/course/arrays/ARRAYS/problems/RUNCOMPARE
+#include<iostream>
+using namespace std;
+int main(void){
+	int t;
+	cin>>t;
+	while(t--){
+		int n =0;
+		int count =0;
+		cin>>n;
+		int arr[2][n];
+		for(int i=0;i<n;i++){
+			cin>>arr[0][i];
+		}for(int j=0;j<n;j++){
+			cin>>arr[1][j];
+		}
+		while(n--){
+			if((arr[0][n]*2>=arr[1][n])&&(arr[1][n]*2>=arr[0][n])){
+				count+=1;
+			}
+		}
+		cout<<count<<"\n";
+	}
+}
