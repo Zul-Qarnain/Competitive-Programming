7fe1ee436e0db0f36aa28305dd78718ca5c7a9aa
Mohammd Shihab Hossain <68942280+Zul-Qarnain@users.noreply.github.com>
2024-08-29 15:35:15 +0600

    Update and rename 11.cpp to 1024.cpp

---
+++ beecrowd/1024.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cctype>
+#include <algorithm>
+int main(void) {
+    int t;
+    std::cin >> t;
+    std::cin.ignore(std::numeric_limits<streamsize>::max(), '\n'); 
+    while (t-- > 0) {
+        std::string input;
+        getline(cin, input);
+        int inputLength = input.length();
+        int num = inputLength / 2;
+        for (int i = 0; i < inputLength; i++) {
+            if (isalpha(input[i])) {
+                input[i] += 3;
+            }
+        }
+        reverse(input.begin(), input.end());
+        for (int i = num; i < inputLength; i++) {
+            input[i] -= 1;
+        }
+        std::cout << input << "\n";
+    }
+}
--- beecrowd/11.cpp
+++
@@ -1,4 +0,0 @@
-#include<iostream>
-int main(void){
-    std::cout<<"HI mom and dad also brother"<<"\n";
-}
