dcc998836cbb05c2047783dd5bc1f6d0b5cc00ee
Mohammd Shihab Hossain <68942280+Zul-Qarnain@users.noreply.github.com>
2025-01-03 13:13:32 +0000

    Bismillah

---
+++ CodeForces/2044_A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void){
+	int t;
+	cin>>t;
+	while(t-->0){
+		int n;
+		cin>>n;
+		if(n<=1){
+			cout<<0<<"\n";
+		}else{
+			cout<<n-1<<"\n";
+		}
+		}
+	} //8 min to solve
